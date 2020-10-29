ba70a98aa0ab9bb5c11075cfaee87c154cc1a200
iwanaga <nagaiwasann@gmail.com>
2020-10-29 20:44:51 +0900

    [update] webpack config.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
 
-mix.js('src/js/app.js', 'dist/js/');
+mix.js('src/js/app.js', 'dist/js/')
+  .sourceMaps(true, 'inline-source-map');
   // .sass('src/sass/app.scss', 'public/css/');
