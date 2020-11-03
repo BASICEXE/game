ab710e78421e4eb76d04b51641106044b95f0ba5
iwanaga <nagaiwasann@gmail.com>
2020-11-03 21:19:24 +0900

    [update] sassの開発環境を追加

--- src/js/app.js
+++ src/js/app.js
@@ -5,7 +5,7 @@ import Main from './_main'
 
 window.onload = function(){
   console.log('読み込み完了')
-  const core = new Core(320, 320)
+  const core = new Core(640, 640)
   Main.init(core)
   Main.facade()
   Main.start()
--- webpack.mix.js
+++ webpack.mix.js
@@ -3,9 +3,9 @@ const mix = require('laravel-mix');
 mix.copyDirectory('src/img', 'dist/img')
   .js('src/js/app.js', 'dist/js/')
   .sourceMaps(true, 'inline-source-map')
+  .sass('src/css/app.scss', 'dist/css/')
   .browserSync({
     files: 'dist/**/*',
     server: 'dist',
     proxy: false
   });
-  // .sass('src/sass/app.scss', 'public/css/');
