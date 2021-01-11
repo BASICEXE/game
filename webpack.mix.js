604c03d1b74fbef126fce7c86e84f8b5d6f828b3
iwanaga <nagaiwasann@gmail.com>
2021-01-11 17:39:05 +0900

    画像アセットをコピー対象に変更

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,8 @@
 const mix = require('laravel-mix');
 
 mix.copyDirectory('src/img', 'dist/img')
+  .copy('src/index.html', 'dist/index.html')
+  .copy('src/images/**/**', 'dist/')
   .js('src/js/app.js', 'dist/js/')
   .sourceMaps(true, 'inline-source-map')
   .sass('src/css/app.scss', 'dist/css/')
